93cea04033088a13ecd1f773d983b63f40787288
modularbond <143904031+modularbond@users.noreply.github.com>
2023-09-06 00:16:04 -0400

    update

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const scrambleChars = "01*";
+const targetText1 = "modular";
+const targetText2 = "bond";
+const durationPerLetter = 40;  // Duration to unscramble each letter (in milliseconds)
+
+const word1Element = document.getElementById("word1");
+const word2Element = document.getElementById("word2");
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function scrambleText(target, revealedCount) {
+  let scrambled = "";
+  for (let i = 0; i < target.length; i++) {
+    if (i < revealedCount) {
+      scrambled += target[i];
+    } else {
+      scrambled += scrambleChars[randomInt(0, scrambleChars.length - 1)];
+    }
+  }
+  return scrambled;
+}
+
+function unscramble(wordElement, targetText, revealedCount) {
+  if (revealedCount > targetText.length) {
+    return;
+  }
+  wordElement.textContent = scrambleText(targetText, revealedCount);
+  setTimeout(() => {
+    unscramble(wordElement, targetText, revealedCount + 1);
+  }, durationPerLetter);
+}
+
+word1Element.textContent = scrambleText(targetText1, 0);
+word2Element.textContent = scrambleText(targetText2, 0);
+
+// Start the unscrambling process
+setTimeout(() => {
+  unscramble(word1Element, targetText1, 0);
+}, 100);  // Initial delay before starting to unscramble
+
+setTimeout(() => {
+  unscramble(word2Element, targetText2, 0);
+}, 100);  // Initial delay before starting to unscramble
