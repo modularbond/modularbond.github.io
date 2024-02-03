e92d30bc24f0fee5ba3652b523132e56016a1bfb
modularbond <143904031+modularbond@users.noreply.github.com>
2024-02-02 23:01:04 -0500

    Update youtube-api.js

--- api/youtube-api.js
+++ api/youtube-api.js
@@ -1,26 +1,22 @@
-// Import the fetch API for Node.js
-const fetch = require('node-fetch');
-
 module.exports = async (req, res) => {
-    // Retrieve the YouTube API key from environment variables
+    let fetch;
+    try {
+        fetch = (await import('node-fetch')).default;
+    } catch (error) {
+        console.error('Error importing node-fetch:', error);
+        return res.status(500).json({ error: 'Failed to import node-fetch' });
+    }
+
     const apiKey = process.env.youtubeAPI;
-    const channelId = 'UCXwQ7RZ8pBb4KO-_MI2ff2w'; // Your YouTube Channel ID
+    const channelId = 'UCXwQ7RZ8pBb4KO-_MI2ff2w';
     const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
 
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-  
-        if (data.items && data.items.length > 0) {
-            // Respond with the live broadcast details
-            res.status(200).json({ live: true, data: data.items });
-        } else {
-            // Respond indicating the channel is not currently live
-            res.status(200).json({ live: false });
-        }
+        const youtubeResponse = await fetch(apiUrl);
+        const youtubeData = await youtubeResponse.json();
+        res.json(youtubeData);
     } catch (error) {
-        console.error('Error fetching YouTube API:', error);
+        console.error('Error calling YouTube API:', error);
         res.status(500).json({ error: 'Failed to fetch data from YouTube API' });
     }
 };
