ab5954bfce4e01528249aae3ff93c879ea298db3
modularbond <143904031+modularbond@users.noreply.github.com>
2024-02-02 22:34:34 -0500

    a

--- api/youtube-api.js
+++ api/youtube-api.js
@@ -1,33 +1,32 @@
-const fetch = require('node-fetch');
-
 const allowCors = fn => async (req, res) => {
+    // Dynamically set the allowed origin based on the request origin
+    const allowedOrigins = ['https://modular.bond', 'https://modularbond-github-o3hgd4j9m-yosephlin.vercel.app'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+  
+    // Set other CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Consider tightening this for production
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
     );
+    
+    // Handle pre-flight requests
     if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
+      res.status(200).end();
+      return;
     }
+  
     return await fn(req, res);
-};
-
-const handler = async (req, res) => {
-    const apiKey = process.env.youtubeAPI; // Make sure to define this in your Vercel project settings
-    const channelId = 'UCXwQ7RZ8pBb4KO-_MI2ff2w';
-    const apiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&eventType=live&type=video&key=${apiKey}`;
-
-    try {
-        const youtubeResponse = await fetch(apiUrl);
-        const youtubeData = await youtubeResponse.json();
-        res.status(200).json(youtubeData);
-    } catch (error) {
-        console.error('YouTube API call failed:', error);
-        res.status(500).json({ error: 'Failed to fetch data from YouTube' });
-    }
-};
-
-module.exports = allowCors(handler);
+  };
+  
+  // Your handler function here
+  const handler = async (req, res) => {
+    // Your existing logic...
+  };
+  
+  module.exports = allowCors(handler);
+  
